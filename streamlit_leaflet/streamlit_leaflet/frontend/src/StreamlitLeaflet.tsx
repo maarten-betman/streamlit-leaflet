01b2957f629e16424d9612d70a07a12b378b044d
Fanilo Andrianasolo <fanilo.andrianasolo@worldline.com>
2021-04-01 12:09:11 +0200

    Use hooks instead

--- streamlit_leaflet/streamlit_leaflet/frontend/src/StreamlitLeaflet.tsx
+++ streamlit_leaflet/streamlit_leaflet/frontend/src/StreamlitLeaflet.tsx
@@ -1,86 +1,81 @@
 import {
   Streamlit,
-  StreamlitComponentBase,
+  ComponentProps,
   withStreamlitConnection,
 } from "streamlit-component-lib"
-import React, {ReactNode, useEffect } from "react"
-import { MapContainer, TileLayer, FeatureGroup } from 'react-leaflet'
+import React, { useEffect } from "react"
+import L from "leaflet"
+import { MapContainer, TileLayer, FeatureGroup } from "react-leaflet"
 import { EditControl } from "react-leaflet-draw"
 
 import "leaflet/dist/leaflet.css"
 import "leaflet-draw/dist/leaflet.draw.css"
 
+import * as marker_icon_2x from "leaflet/dist/images/marker-icon-2x.png"
+import * as marker_icon from "leaflet/dist/images/marker-icon.png"
+import * as shadow_icon from "leaflet/dist/images/marker-shadow.png"
+
+export interface PythonArgs {
+  map_center: number[]
+  map_zoom: number
+}
 
 const mapStyle: React.CSSProperties = {
-    height: '600px'
+  height: "600px",
 }
 
+const StreamlitLeaflet = ({ args }: ComponentProps) => {
+  const { map_center, map_zoom }: PythonArgs = args
 
-class StreamlitLeaflet extends StreamlitComponentBase {
-
-    _onCreated = (e: any) => {
-    let type = e.layerType;
-    if (type === 'polyline') {
+  const _onCreated = (e: any) => {
+    let type = e.layerType
+    if (type === "polyline") {
       // Do marker specific actions
-      console.log('_onCreated: marker created', e);
+      console.log("_onCreated: marker created", e)
     } else {
-      console.log('_onCreated: something else created:', type, e);
+      console.log("_onCreated: something else created:", type, e)
     }
     // Do whatever else you need to. (save to db; etc)
-        Streamlit.setComponentValue(e.layer.editing.latlngs)
-        Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
-
-        Streamlit.setComponentReady()
-        Streamlit.setFrameHeight(600)
-
-        console.log(e.layer.editing.latlngs)
-
-  };
-
-    public render = (): ReactNode => {
-
-        const L = require("leaflet");
-        delete L.Icon.Default.prototype._getIconUrl;
-        L.Icon.Default.mergeOptions({
-            iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-            iconUrl: require("leaflet/dist/images/marker-icon.png"),
-            shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-        });
-
-        const mapCenter = this.props.args["map_center"]
-        const mapZoom = this.props.args["map_zoom"]
-
-        return (
-            <MapContainer
-                center={mapCenter}
-                zoom={mapZoom}
-                scrollWheelZoom={true}
-                style={mapStyle}
-            >
-                  <FeatureGroup>
-                    <EditControl
-                      position='topleft'
-                      onCreated={this._onCreated}
-                      draw={{
-                          rectangle: false,
-                          circle: false,
-                          polygon: false,
-                          marker: false,
-                          circlemarker: false
-                      }}
-                    />
-                  </FeatureGroup>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-            </MapContainer>
-    )
+    Streamlit.setComponentValue(e.layer.editing.latlngs)
+    console.log(e.layer.editing.latlngs)
   }
+
+  useEffect(() => {
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: marker_icon_2x,
+      iconUrl: marker_icon,
+      shadowUrl: shadow_icon,
+    })
+  })
+
+  useEffect(() => Streamlit.setFrameHeight(600))
+
+  return (
+    <MapContainer
+      center={[map_center[0], map_center[1]]}
+      zoom={map_zoom}
+      scrollWheelZoom={true}
+      style={mapStyle}
+    >
+      <FeatureGroup>
+        <EditControl
+          position="topleft"
+          onCreated={_onCreated}
+          draw={{
+            rectangle: false,
+            circle: false,
+            polygon: false,
+            marker: false,
+            circlemarker: false,
+          }}
+        />
+      </FeatureGroup>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  )
 }
 
-function onRender(event: Event): void {
-    Streamlit.setFrameHeight(600)
-}
-
-export default withStreamlitConnection(StreamlitLeaflet);
+export default withStreamlitConnection(StreamlitLeaflet)
